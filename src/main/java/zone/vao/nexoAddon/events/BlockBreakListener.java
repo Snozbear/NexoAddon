744a4c747ecfb8df757ea57dbae3af45ff9bc932
Naimadx123 <damianx787@gmail.com>
2024-12-29 19:16:23 +0100

    feat(BlockBreakListener): fix of barriers not reconstructing immediately on double_hit_destroy_mechanic

--- src/main/java/zone/vao/nexoAddon/events/BlockBreakListener.java
+++ src/main/java/zone/vao/nexoAddon/events/BlockBreakListener.java
@@ -3,6 +3,7 @@ package zone.vao.nexoAddon.events;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import zone.vao.nexoAddon.NexoAddon;
 import zone.vao.nexoAddon.events.blockBreaks.JukeboxSupport;
 import zone.vao.nexoAddon.events.blockBreaks.ShearsBreak;
 
@@ -13,5 +14,6 @@ public class BlockBreakListener implements Listener {
 
     JukeboxSupport.onBlockBreak(event);
     ShearsBreak.onBlockBreak(event);
+    event.setCancelled(NexoAddon.getInstance().getGlobalConfig().getBoolean("double_hit_destroy_mechanic", true));
   }
 }
