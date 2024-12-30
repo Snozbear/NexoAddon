9cc6d5368bc9fba19068cdacf6bb603f2129899f
Naimadx123 <damianx787@gmail.com>
2024-12-30 15:08:51 +0100

    feat: Simplify PlayerMovementListener imports

--- src/main/java/zone/vao/nexoAddon/events/PlayerMovementListener.java
+++ src/main/java/zone/vao/nexoAddon/events/PlayerMovementListener.java
@@ -1,20 +1,10 @@
 package zone.vao.nexoAddon.events;
 
-import com.nexomc.nexo.api.NexoItems;
-import com.nexomc.nexo.items.ItemBuilder;
-import com.nexomc.nexo.mechanics.furniture.FurnitureMechanic;
-import net.kyori.adventure.text.TextComponent;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BarStyle;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.scheduler.BukkitRunnable;
 import zone.vao.nexoAddon.NexoAddon;
 import zone.vao.nexoAddon.events.playerMovements.FurnituresRaytrace;
-import zone.vao.nexoAddon.utils.BossBarUtil;
-import zone.vao.nexoAddon.utils.RayTraceUtil;
 
 public class PlayerMovementListener implements Listener {
 
