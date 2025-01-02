75061bed7a3a4e8e97ed2d40800acb8bc82c4617
Naimadx123 <damianx787@gmail.com>
2025-01-02 14:11:14 +0100

    feat(DisplayCropsHologram): add validation for furniture evolution existence

--- src/main/java/zone/vao/nexoAddon/events/playerFurnitureInteracts/DisplayCropsHologram.java
+++ src/main/java/zone/vao/nexoAddon/events/playerFurnitureInteracts/DisplayCropsHologram.java
@@ -2,6 +2,7 @@ package zone.vao.nexoAddon.events.playerFurnitureInteracts;
 
 import com.nexomc.nexo.api.NexoFurniture;
 import com.nexomc.nexo.api.events.furniture.NexoFurnitureInteractEvent;
+import com.nexomc.nexo.mechanics.furniture.FurnitureMechanic;
 import org.bukkit.persistence.PersistentDataType;
 import zone.vao.nexoAddon.utils.HologramUtil;
 
@@ -10,6 +11,10 @@ import static zone.vao.nexoAddon.events.playerFurnitureInteracts.Fertilize.EVOLU
 public class DisplayCropsHologram {
 
   public static void onInteract(NexoFurnitureInteractEvent event){
+
+    FurnitureMechanic furniture = NexoFurniture.furnitureMechanic(event.getBaseEntity());
+
+    if(furniture == null || furniture.getEvolution() == null) return;
 
     double progress = (double) event.getBaseEntity().getPersistentDataContainer().get(EVOLUTION_KEY, PersistentDataType.INTEGER)
         / NexoFurniture.furnitureMechanic(event.getBaseEntity()).getEvolution().getDelay();;
