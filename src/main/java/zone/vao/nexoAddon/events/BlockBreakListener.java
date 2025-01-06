2620d6957cdd764755caa677f3dc2c892ce6d4ae
Naimadx123 <damianx787@gmail.com>
2025-01-06 20:01:31 +0100

    refactor: remove unused imports in BlockBreakListener.java

--- src/main/java/zone/vao/nexoAddon/events/BlockBreakListener.java
+++ src/main/java/zone/vao/nexoAddon/events/BlockBreakListener.java
@@ -1,7 +1,5 @@
 package zone.vao.nexoAddon.events;
 
-import com.nexomc.nexo.api.NexoFurniture;
-import io.th0rgal.protectionlib.ProtectionLib;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
