248abfa7bfb1c1beee49485df99d870cf44097bf
Naimadx123 <damianx787@gmail.com>
2025-01-15 00:42:01 +0100

    feat: update hologram utility to use Component for progressBar

--- src/main/java/zone/vao/nexoAddon/utils/HologramUtil.java
+++ src/main/java/zone/vao/nexoAddon/utils/HologramUtil.java
@@ -1,8 +1,7 @@
 package zone.vao.nexoAddon.utils;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.ArmorStand;
@@ -11,7 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import zone.vao.nexoAddon.NexoAddon;
 
-import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -24,15 +22,13 @@ public class HologramUtil {
     if (entity == null || progress < 0.0 || progress > 1.0) return;
 
     new BukkitRunnable() {
-
       @Override
       public void run() {
         World world = entity.getWorld();
         Location entityLocation = entity.getLocation().clone();
         Location hologramLocation = entityLocation.add(0, 0.5, 0);
 
-        String progressBar = getProgressBar(progress, 10);
-        String hologramText = NamedTextColor.GREEN + progressBar;
+        Component progressBar = getProgressBar(progress, 10);
 
         new BukkitRunnable() {
           @Override
@@ -42,8 +38,9 @@ public class HologramUtil {
               existingHologram.remove();
               holograms.remove(player.getUniqueId());
             }
+
             ArmorStand hologram = world.spawn(hologramLocation, ArmorStand.class, stand -> {
-              stand.setCustomName(hologramText);
+              stand.customName(progressBar);
               stand.setCustomNameVisible(true);
               stand.setGravity(false);
               stand.setInvisible(true);
@@ -54,14 +51,15 @@ public class HologramUtil {
                 player.showEntity(NexoAddon.getInstance(), stand);
               }
             });
-            if(player != null)
+
+            if (player != null)
               holograms.put(player.getUniqueId(), hologram);
 
             new BukkitRunnable() {
               @Override
               public void run() {
                 hologram.remove();
-                if(player != null)
+                if (player != null)
                   holograms.remove(player.getUniqueId());
               }
             }.runTaskLater(NexoAddon.getInstance(), 60);
@@ -71,22 +69,14 @@ public class HologramUtil {
     }.runTaskAsynchronously(NexoAddon.getInstance());
   }
 
-  private static String getProgressBar(double progress, int length) {
+  private static Component getProgressBar(double progress, int length) {
     int filledLength = (int) (progress * length);
     int emptyLength = length - filledLength;
 
-    StringBuilder bar = new StringBuilder();
-    if(filledLength >= 10 && emptyLength <= 0){
-      bar.append(NamedTextColor.DARK_GREEN);
-      bar.append("█".repeat(filledLength));
-    }
-    else {
-      bar.append(NamedTextColor.GREEN);
-      bar.append("█".repeat(filledLength));
-      bar.append(NamedTextColor.RED);
-      bar.append("█".repeat(emptyLength));
-    }
+    Component filledPart = Component.text("█".repeat(filledLength)).color(NamedTextColor.DARK_GREEN);
+    Component emptyPart = Component.text("█".repeat(emptyLength)).color(NamedTextColor.RED);
 
-    return bar.toString();
+    return filledPart.append(emptyPart);
   }
+
 }
