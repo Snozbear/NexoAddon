67bbdf72ceebe5d4ad278e9572b2161f94937cc6
Naimadx123 <damianx787@gmail.com>
2025-01-18 12:04:13 +0100

    feat: MiningTools for chorusplants type

--- src/main/java/zone/vao/nexoAddon/events/NexoBlockBreakListener.java
+++ src/main/java/zone/vao/nexoAddon/events/NexoBlockBreakListener.java
@@ -1,5 +1,6 @@
 package zone.vao.nexoAddon.events;
 
+import com.nexomc.nexo.api.events.custom_block.chorusblock.NexoChorusBlockBreakEvent;
 import com.nexomc.nexo.api.events.custom_block.noteblock.NexoNoteBlockBreakEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,10 +11,16 @@ import zone.vao.nexoAddon.events.playerNexoBlockBreaks.MiningToolsListener;
 public class NexoBlockBreakListener implements Listener {
 
   @EventHandler
-  public void onBlockBreak(NexoNoteBlockBreakEvent event) {
+  public void onNoteBlockBreak(NexoNoteBlockBreakEvent event) {
 
     MiningToolsListener.onBreak(event);
     DropExperienceListener.onBreak(event);
     InfestedListener.onBreak(event);
+  }
+
+  @EventHandler
+  public void onChorusBreak(NexoChorusBlockBreakEvent event) {
+
+    MiningToolsListener.onBreak(event);
   }
 }
