588e8b2fd756cd2574e3df574f96021e76ee0cf9
Naimadx123 <damianx787@gmail.com>
2025-03-01 14:06:53 +0100

    fix: not working shiftblock mechanic for custom blocks

--- src/main/java/zone/vao/nexoAddon/items/mechanics/ShiftBlock.java
+++ src/main/java/zone/vao/nexoAddon/items/mechanics/ShiftBlock.java
@@ -3,15 +3,23 @@ package zone.vao.nexoAddon.items.mechanics;
 import com.nexomc.nexo.api.NexoBlocks;
 import com.nexomc.nexo.api.NexoFurniture;
 import com.nexomc.nexo.api.NexoItems;
+import com.nexomc.nexo.api.events.custom_block.NexoBlockBreakEvent;
 import com.nexomc.nexo.api.events.custom_block.NexoBlockInteractEvent;
+import com.nexomc.nexo.api.events.custom_block.NexoBlockPlaceEvent;
+import com.nexomc.nexo.api.events.custom_block.chorusblock.NexoChorusBlockBreakEvent;
+import com.nexomc.nexo.api.events.custom_block.chorusblock.NexoChorusBlockInteractEvent;
+import com.nexomc.nexo.api.events.custom_block.chorusblock.NexoChorusBlockPlaceEvent;
 import com.nexomc.nexo.api.events.custom_block.noteblock.NexoNoteBlockBreakEvent;
+import com.nexomc.nexo.api.events.custom_block.noteblock.NexoNoteBlockInteractEvent;
 import com.nexomc.nexo.api.events.custom_block.noteblock.NexoNoteBlockPlaceEvent;
+import com.nexomc.nexo.api.events.custom_block.stringblock.NexoStringBlockBreakEvent;
+import com.nexomc.nexo.api.events.custom_block.stringblock.NexoStringBlockInteractEvent;
+import com.nexomc.nexo.api.events.custom_block.stringblock.NexoStringBlockPlaceEvent;
 import com.nexomc.nexo.api.events.furniture.NexoFurnitureBreakEvent;
 import com.nexomc.nexo.api.events.furniture.NexoFurnitureInteractEvent;
 import com.nexomc.nexo.api.events.furniture.NexoFurniturePlaceEvent;
 import com.nexomc.nexo.mechanics.custom_block.CustomBlockMechanic;
 import com.nexomc.nexo.mechanics.furniture.FurnitureMechanic;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -34,7 +42,7 @@ public record ShiftBlock(String replaceTo, int time, List<Material> materials, L
     public static List<UUID> toCancelation = Collections.synchronizedList(new ArrayList<>());
 
     @EventHandler
-    public static void onShiftBlockPlace(NexoFurniturePlaceEvent event) {
+    public static void onShiftBlockPlaceFurniture(NexoFurniturePlaceEvent event) {
       if (toCancelation.contains(event.getPlayer().getUniqueId())) {
         event.setCancelled(true);
         toCancelation.remove(event.getPlayer().getUniqueId());
@@ -57,7 +65,7 @@ public record ShiftBlock(String replaceTo, int time, List<Material> materials, L
     }
 
     @EventHandler(priority = EventPriority.HIGH)
-    public static void onShiftBlockInteract(NexoFurnitureInteractEvent event) {
+    public static void onShiftBlockInteractFurniture(NexoFurnitureInteractEvent event) {
       if (event.getHand() != EquipmentSlot.HAND)
         return;
       Mechanics mechanics = NexoAddon.getInstance().getMechanics().get(event.getMechanic().getItemID());
@@ -81,7 +89,7 @@ public record ShiftBlock(String replaceTo, int time, List<Material> materials, L
     }
 
     @EventHandler
-    public static void onShiftBlockBreak(NexoFurnitureBreakEvent event) {
+    public static void onShiftBlockBreakFurniture(NexoFurnitureBreakEvent event) {
       Mechanics mechanics = NexoAddon.getInstance().getMechanics().get(event.getMechanic().getItemID());
       if (mechanics == null || mechanics.getShiftBlock() == null || !mechanics.getShiftBlock().onBreak())
         return;
@@ -100,40 +108,50 @@ public record ShiftBlock(String replaceTo, int time, List<Material> materials, L
 
     @EventHandler
     public static void onShiftBlockPlace(NexoNoteBlockPlaceEvent event) {
-      Mechanics mechanics = NexoAddon.getInstance().getMechanics().get(event.getMechanic().getItemID());
-      if (mechanics == null || mechanics.getShiftBlock() == null || !mechanics.getShiftBlock().onPlace())
-        return;
-      CustomBlockMechanic customBlockMechanic = NexoBlocks.customBlockMechanic(mechanics.getShiftBlock().replaceTo());
-      if (customBlockMechanic == null)
-        return;
-      ItemStack itemStack = event.getPlayer().getInventory().getItemInMainHand();
-      if (!mechanics.getShiftBlock().materials().isEmpty() &&
-          mechanics.getShiftBlock().materials().contains(itemStack.getType()) &&
-          !mechanics.getShiftBlock().nexoIds().isEmpty() &&
-          !mechanics.getShiftBlock().nexoIds().contains(NexoItems.idFromItem(itemStack)))
-        return;
-      startShiftBlock(event.getBlock().getLocation(), customBlockMechanic, event.getMechanic(), mechanics.getShiftBlock().time());
+      handlePlaceCustomBlock(event);
+    }
+
+    @EventHandler
+    public static void onShiftBlockPlace(NexoStringBlockPlaceEvent event) {
+      handlePlaceCustomBlock(event);
+    }
+
+    @EventHandler
+    public static void onShiftBlockPlace(NexoChorusBlockPlaceEvent event) {
+      handlePlaceCustomBlock(event);
     }
 
     @EventHandler
     public static void onShiftBlockBreak(NexoNoteBlockBreakEvent event) {
-      Mechanics mechanics = NexoAddon.getInstance().getMechanics().get(event.getMechanic().getItemID());
-      if (mechanics == null || mechanics.getShiftBlock() == null || !mechanics.getShiftBlock().onBreak())
-        return;
-      CustomBlockMechanic customBlockMechanic = NexoBlocks.customBlockMechanic(mechanics.getShiftBlock().replaceTo());
-      if (customBlockMechanic == null)
-        return;
-      ItemStack itemStack = event.getPlayer().getInventory().getItemInMainHand();
-      if (!mechanics.getShiftBlock().materials().isEmpty() &&
-          mechanics.getShiftBlock().materials().contains(itemStack.getType()) &&
-          !mechanics.getShiftBlock().nexoIds().isEmpty() &&
-          !mechanics.getShiftBlock().nexoIds().contains(NexoItems.idFromItem(itemStack)))
-        return;
-      startShiftBlock(event.getBlock().getLocation(), customBlockMechanic, event.getMechanic(), mechanics.getShiftBlock().time());
+      handleBreakCustomBlock(event);
     }
 
     @EventHandler
-    public static void onShiftBlockInteract(NexoBlockInteractEvent event) {
+    public static void onShiftBlockBreak(NexoStringBlockBreakEvent event) {
+      handleBreakCustomBlock(event);
+    }
+
+    @EventHandler
+    public static void onShiftBlockBreak(NexoChorusBlockBreakEvent event) {
+      handleBreakCustomBlock(event);
+    }
+
+    @EventHandler
+    public static void onShiftBlockInteract(NexoNoteBlockInteractEvent event) {
+      handleInteractCustomBlock(event);
+    }
+
+    @EventHandler
+    public static void onShiftBlockInteract(NexoStringBlockInteractEvent event) {
+      handleInteractCustomBlock(event);
+    }
+
+    @EventHandler
+    public static void onShiftBlockInteract(NexoChorusBlockInteractEvent event) {
+      handleInteractCustomBlock(event);
+    }
+
+    private static void handleInteractCustomBlock(NexoBlockInteractEvent event) {
       if (event.getHand() != EquipmentSlot.HAND)
         return;
       Mechanics mechanics = NexoAddon.getInstance().getMechanics().get(event.getMechanic().getItemID());
@@ -150,6 +168,38 @@ public record ShiftBlock(String replaceTo, int time, List<Material> materials, L
         return;
 
       event.getPlayer().swingMainHand();
+      startShiftBlock(event.getBlock().getLocation(), customBlockMechanic, event.getMechanic(), mechanics.getShiftBlock().time());
+    }
+
+    private static void handleBreakCustomBlock(NexoBlockBreakEvent event) {
+      Mechanics mechanics = NexoAddon.getInstance().getMechanics().get(event.getMechanic().getItemID());
+      if (mechanics == null || mechanics.getShiftBlock() == null || !mechanics.getShiftBlock().onBreak())
+        return;
+      CustomBlockMechanic customBlockMechanic = NexoBlocks.customBlockMechanic(mechanics.getShiftBlock().replaceTo());
+      if (customBlockMechanic == null)
+        return;
+      ItemStack itemStack = event.getPlayer().getInventory().getItemInMainHand();
+      if (!mechanics.getShiftBlock().materials().isEmpty() &&
+          mechanics.getShiftBlock().materials().contains(itemStack.getType()) &&
+          !mechanics.getShiftBlock().nexoIds().isEmpty() &&
+          !mechanics.getShiftBlock().nexoIds().contains(NexoItems.idFromItem(itemStack)))
+        return;
+      startShiftBlock(event.getBlock().getLocation(), customBlockMechanic, event.getMechanic(), mechanics.getShiftBlock().time());
+    }
+
+    private static void handlePlaceCustomBlock(NexoBlockPlaceEvent event){
+      Mechanics mechanics = NexoAddon.getInstance().getMechanics().get(event.getMechanic().getItemID());
+      if (mechanics == null || mechanics.getShiftBlock() == null || !mechanics.getShiftBlock().onPlace())
+        return;
+      CustomBlockMechanic customBlockMechanic = NexoBlocks.customBlockMechanic(mechanics.getShiftBlock().replaceTo());
+      if (customBlockMechanic == null)
+        return;
+      ItemStack itemStack = event.getPlayer().getInventory().getItemInMainHand();
+      if (!mechanics.getShiftBlock().materials().isEmpty() &&
+          mechanics.getShiftBlock().materials().contains(itemStack.getType()) &&
+          !mechanics.getShiftBlock().nexoIds().isEmpty() &&
+          !mechanics.getShiftBlock().nexoIds().contains(NexoItems.idFromItem(itemStack)))
+        return;
       startShiftBlock(event.getBlock().getLocation(), customBlockMechanic, event.getMechanic(), mechanics.getShiftBlock().time());
     }
   }
