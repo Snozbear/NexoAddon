3f4b86d5cae2893a9d03d6014c68937f9ba54095
Naimadx123 <damianx787@gmail.com>
2025-05-27 18:40:50 +0200

    refactor: move NALoader and Repo to loader package

--- src/main/java/zone/vao/nexoAddon/loader/NALoader.java
+++ src/main/java/zone/vao/nexoAddon/loader/NALoader.java
@@ -1,4 +1,4 @@
-package zone.vao.nexoAddon;
+package zone.vao.nexoAddon.loader;
 
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
@@ -8,8 +8,6 @@ import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
 
 import java.util.List;
-
-record Repo(String id, String url){}
 
 public class NALoader implements PluginLoader {
   private static final List<Repo> REPOS =
